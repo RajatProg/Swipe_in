7a0bd5e25a64d0ac1beb049b66931be5245cef3a
RajatProg <rparida0105@my.msutexas.edu>
2025-03-17 02:29:10 -0500

    4th Commit before iteration 2

--- app/styles/cfa_images.tsx
+++ app/styles/cfa_images.tsx
@@ -8,27 +8,43 @@ export const cfaImages: Record<string, any> = {
     "Fruit Cup": require('../../assets/images/fruit.png'),
     "Cookies & Cream Milkshake": require('../../assets/images/CFA_Deluxe.png'),
     "Strawberry Milkshake": require('../../assets/images/regular.png'),
-    "Vanilla Milkshake (Lg)": require('../../assets/images/spicy.png'),
-    "Icedream": require('../../assets/images/spicy_deluxe.png'),
-    "Freshly-Brewed Iced Tea Unsweetened": require('../../assets/images/CFA_Deluxe.png'),
-    "Chick-fil-A® Lemonade": require('../../assets/images/regular.png'),
-    "Chick-fil-A® Diet Lemonade": require('../../assets/images/spicy.png'),
-    "Grilled Chicken Sandwich": require('../../assets/images/spicy_deluxe.png'),
+    "Vanilla Milkshake (Lg)": require('../../assets/images/milkshake.png'),
+    "Icedream": require('../../assets/images/icedream.png'),
+
+    "Freshly-Brewed Iced Tea Unsweetened": require('../../assets/images/unsweet.png'),
+    "Chick-fil-A® Lemonade": require('../../assets/images/lemonade.png'),
+
+    "Chick-fil-A® Diet Lemonade": require('../../assets/images/lemonade.png'),
+
+    "Grilled Chicken Sandwich": require('../../assets/images/grill.png'),
     "Grilled Chicken Club": require('../../assets/images/grill_colby.png'),
-    "Chick-fil-A® Sauce": require('../../assets/images/regular.png'),
-    "Polynesian Sauce": require('../../assets/images/spicy.png'),
-    "Honey Mustard Sauce": require('../../assets/images/spicy_deluxe.png'),
-    "Garden Herb Ranch Sauce": require('../../assets/images/CFA_Deluxe.png'),
-    "Zesty Buffalo Sauce": require('../../assets/images/regular.png'),
-    "Barbeque Sauce": require('../../assets/images/spicy.png'),
-    "Sweet & Spicy Sriracha Sauce": require('../../assets/images/spicy_deluxe.png'),
-    "Tomatoes": require('../../assets/images/CFA_Deluxe.png'),
-    "Lettuce": require('../../assets/images/regular.png'),
-    "American Cheese": require('../../assets/images/spicy.png'),
-    "Colby Jack Cheese": require('../../assets/images/spicy_deluxe.png'),
-    "Pepper Jack Cheese": require('../../assets/images/CFA_Deluxe.png'),
-    "Pickles": require('../../assets/images/CFA_Deluxe.png'),
-    "No Pickles": require('../../assets/images/regular.png')
+
+    "Chick-fil-A® Sauce": require('../../assets/images/cfa.png'),
+
+    "Polynesian Sauce": require('../../assets/images/poly.png'),
+
+    "Honey Mustard Sauce": require('../../assets/images/honey.png'),
+    "Garden Herb Ranch Sauce": require('../../assets/images/herb.png'),
+    "Zesty Buffalo Sauce": require('../../assets/images/buffalo.png'),
+
+    "Barbeque Sauce": require('../../assets/images/barbeque.png'),
+    "Sweet & Spicy Sriracha Sauce": require('../../assets/images/siracha.png'),
+
+    "Tomatoes": require('../../assets/images/tomato.png'),
+    "Lettuce": require('../../assets/images/lettuce.png'),
+    "American Cheese": require('../../assets/images/ac.jpeg'),
+    "Colby Jack Cheese": require('../../assets/images/cj.jpeg'),
+    "Pepper Jack Cheese": require('../../assets/images/ppr.jpg'),
+    "Pickles": require('../../assets/images/pick.png'),
+    "No Pickles": require('../../assets/images/pick.png'),
+
+
+    "Chick-fil-A® Nuggets (8 count)": require('../../assets/images/nuggets_8.png'),
+    "Chick-fil-A® Nuggets (12 count)": require('../../assets/images/12count.png'),
+    "Grilled Nuggets (8 count)": require('../../assets/images/8countgrill.png'),
+    "Grilled Nuggets (12 count)": require('../../assets/images/8countgrill.png'),
+    'Fountain Drink (Large)': require('../../assets/images/cfa.webp'),
+    'Fountain Drink (Medium)': require('../../assets/images/cfa.webp')
        
     
   };
