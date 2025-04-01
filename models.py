214862304859901206b77d10cb6a1cfad8aebe17
RajatProg <rparida0105@my.msutexas.edu>
2025-04-01 08:35:01 -0500

    model change

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,String,Integer,Float,Boolean,Date,ForeignKey,DateTime, func
+from sqlalchemy import Column,String,Integer,Float,Boolean,Date,ForeignKey,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
